e366c921d943fada684260520e0d1254061ef630
diegodov <diegodd07@gmail.com>
2024-01-30 05:13:44 -0300

    Agregado contrato e implementacion

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,6 +1,9 @@
 using Ecommerce.Repositorio.DBContext;
 using Microsoft.EntityFrameworkCore;
 
+using Ecommerce.Repositorio.Contrato;
+using Ecommerce.Repositorio.Implementacion;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -15,6 +18,9 @@ builder.Services.AddDbContext<DbecommerceContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
 });
+
+builder.Services.AddTransient(typeof(IGenericoRepositorio<>), typeof(GenericoRepositorio<>));
+builder.Services.AddScoped<IVentaRepositorio, VentaRepositorio>();
 
 var app = builder.Build();
 
